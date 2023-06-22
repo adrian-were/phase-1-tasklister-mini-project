5fffc189a211fb561245df9c779e8f91d002f83d
Adrian <adrianshisia@gmail.com>
2023-06-22 14:45:19 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.querySelector('form').addEventListener('submit', (e) => {
+e.preventDefault()
+toPrint(e.target.new-task-description.value)
+})
 });
+
+function toPrint() {
+
+}
